56245b2446520d194da7e819c2c60104ce75843a
Michal Nowotka <mnowotka@ebi.ac.uk>
2014-06-13 16:57:31 +0100

    new version

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ except ImportError:
     use_setuptools()
     from setuptools import setup
 
-if sys.version_info < (2, 7, 3) or sys.version_info >= (2, 7, 6):
-    raise Exception('ChEMBL software stack requires python 2.7.3 - 2.7.5')
+if sys.version_info < (2, 7, 3) or sys.version_info >= (2, 7, 7):
+    raise Exception('ChEMBL software stack requires python 2.7.3 - 2.7.7')
 
 setup(
     name='chembl_migration_model',
-    version='0.5.2',
+    version='0.5.3',
     author='Michal Nowotka',
     author_email='mnowotka@ebi.ac.uk',
     description='Core ChEMBL python ORM model for data exports and migration',
